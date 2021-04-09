da9f16c6c333f43f25fbb908c195dc878c4518eb
akihide sato <akihide315@gmail.com>
2021-04-09 20:21:56 +0900

    Fix: enum定義

---
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/types/Price.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.bizfw.basic.buyticket.types;
+
+/**
+ * @author jflute
+ * @author sato_akihide
+ */
+public enum Price {
+    ONE_DAY(7400), TWO_DAY(13200), FOUR_DAY(22400);
+
+    private int value;
+
+    private Price(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/org/docksidestage/bizfw/basic/buyticket/types/TicketType.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.docksidestage.bizfw.basic.buyticket.types;
+
+/**
+ * @author jflute
+ * @author sato_akihide
+ */
+public enum TicketType {
+
+    ONE_DAY(Price.ONE_DAY, DaysType.ONE_DAY), TWO_DAY(Price.TWO_DAY, DaysType.TWO_DAY), FOUR_DAY(Price.TWO_DAY, DaysType.FOUR_DAY);
+
+    private Price price;
+    private DaysType daysType;
+
+    private TicketType(Price price, DaysType daysType) {
+        this.price = price;
+        this.daysType = daysType;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public DaysType getDaysType() {
+        return daysType;
+    }
+}
